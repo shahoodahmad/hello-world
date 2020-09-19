3337f45c372b3a4e5ab353db05ae9aa328500ae0
Shahood Ahmad <54773167+shahoodahmad@users.noreply.github.com>
2020-09-18 23:34:55 -0400

    weather api

--- weather.js
+++ weather.js
@@ -1,25 +1,69 @@
+// access all the elements to be formatted
+var notification = document.getElementById("notification");
+var descPic = document.getElementById("descriptive-picture");
+var temp = document.getElementById("temperature");
+var weatherDesc = document.getElementById("weather-description");
+var location = document.getElementById("location");
 
-var x = document.getElementById("notification");
-
+// use the html geolocator api to find the user's location
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition, showError);
   }
   else {
-    x.innerHTML = "Geolocation not supported";
+    notification.innerHTML = "Geolocation not supported";
     ;
   }
 }
 
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude +
-  "<br>Longitude: " + position.coords.longitude;
+// if access allowed, store the position and call the weather api function
+function setPosition(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+  getWeather(latitude, longitude);
 }
 
+// if location blocked, display a notification
 function showError(error) {
     if(error.PERMISSION_DENIED){}
-      x.innerHTML = "Location access not granted <br> Reload the page or reset your location settings <br> to give location access and use the app"
+      notification.innerHTML = "Location access not granted <br> Reload the page or reset your location settings <br> to give location access and use the app"
     }
 
+// create a weather object to store the api information
+const weather = {};
 
-  
+// set temperature unit to celcius (default)
+weather.temperature = {
+  unit = "celcius"
+}
+
+// weather api call function
+function getWeather(latitude, longitude){
+  let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=09ca28f467aa94c207b9ce9c95473bc9`;
+
+      fetch(api)
+          .then(function(response){
+              let data = response.json();
+              return data;
+          })
+          .then(function(data){
+              weather.temperature.value = Math.floor(data.main.temp - 273);
+              weather.description = data.weather[0].description;
+              weather.iconId = data.weather[0].icon;
+              weather.city = data.name;
+              weather.country = data.sys.country;
+          })
+          .then(function(){
+              outputWeather();
+          });
+  }
+}
+
+// outputWeather function of chage the html
+function outputWeather(){
+    notification.innerHTML = "Click the temperature unit to convert to another unit";
+    descPic.innerHTML = `<img src="images/icons/${weather.iconId}.png"/>`;
+    temp.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    weatherDesc.innerHTML = weather.description;
+    location.innerHTML = `${weather.city}, ${weather.country}`;
+}
